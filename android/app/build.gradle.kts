5cfcb6c3ce05ee95874096bed146b60e12b10715
gitcoder-23 <prabhatece21@gmail.com>
2025-12-14 11:01:24 +0530

    added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.martfury"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
